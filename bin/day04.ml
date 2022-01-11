9f93cf30cb327fa6a1dde00193abe912e61111bd
Damien Ragoucy <dam@ramdam.io>
2022-01-12 00:29:18 +0100

    Add day 4 part 2

--- bin/day04.ml
+++ bin/day04.ml
@@ -13,6 +13,7 @@ let matrix_size = 5
 let numbers = data |> List.hd_exn |> String.split ~on:',' |> List.map ~f:Int.of_string
 
 type board = int array array
+type boards = board list
 type state = Pending | Reading
 type line_acc = {
   numbers : int list;
@@ -77,37 +78,73 @@ let get_line (board : board) line =
 let get_column (board : board) column =
   Array.map board ~f:(fun line -> Array.get line column)
 
+let is_wining_board board numbers_set =
+  let rec is_wining_board' board numbers_set i =
+    if i >= matrix_size then false
+    else if Set.is_subset (get_line board i |> Set.of_array (module Int))
+        ~of_:numbers_set then true
+    else if Set.is_subset (get_column board i |> Set.of_array (module Int))
+        ~of_:numbers_set then true
+    else is_wining_board' board numbers_set (i + 1)
+  in
+  is_wining_board' board numbers_set 0
+
+let calculate_score board drawn_numbers_set last_number =
+  let board_numbers =
+    Array.map board ~f:(fun line -> Set.of_array (module Int) line)
+    |> Array.fold ~init:(Set.empty (module Int)) ~f:Set.union
+  in
+  Set.diff board_numbers drawn_numbers_set
+  |> Set.fold ~init:0 ~f:(+)
+  |> ( * ) last_number
+  |> Option.some
+
+type board_traversal = {
+  pending : boards;
+  wining : boards;
+}
+
 let result1 =
   let rec try_number numbers boards drawn_numbers_set =
     match numbers with
     | [] -> None
     | drawn_number :: rest ->
       let drawn_numbers_set' = Set.add drawn_numbers_set drawn_number in
-      let rec test_line_and_col boards' i =
-        if i >= matrix_size
-        then try_number rest boards drawn_numbers_set'
-        else
-          match boards' with
-          | [] -> test_line_and_col boards (i + 1)
-          | board :: rest_boards ->
-            if Set.is_subset (get_line board i |> Set.of_array (module Int))
-                ~of_:drawn_numbers_set'
-            then Some (drawn_number, drawn_numbers_set', board)
-            else if Set.is_subset (get_column board i |> Set.of_array (module Int))
-                ~of_:drawn_numbers_set'
-            then Some (drawn_number, drawn_numbers_set', board)
-            else test_line_and_col rest_boards i
-      in
-      test_line_and_col boards 0
+      List.fold boards
+        ~init:{pending = []; wining = []}
+        ~f:(fun ({wining; _} as acc) board ->
+            if is_wining_board board drawn_numbers_set'
+            then {acc with wining = board :: wining}
+            else acc)
+      |> (fun {wining; _} ->
+          if List.is_empty wining
+          then try_number rest boards drawn_numbers_set'
+          else Some (drawn_number, drawn_numbers_set', List.last_exn wining))
   in
   match try_number numbers boards (Set.empty (module Int)) with
   | None -> None
-  | Some (drawn_number, drawn_numbers_set,  board) ->
-    let board_numbers =
-      Array.map board ~f:(fun line -> Set.of_array (module Int) line)
-      |> Array.fold ~init:(Set.empty (module Int)) ~f:Set.union
-    in
-    Set.diff board_numbers drawn_numbers_set
-    |> Set.fold ~init:0 ~f:(+)
-    |> ( * ) drawn_number
-    |> Option.some
+  | Some (drawn_number, drawn_numbers_set, board) ->
+    calculate_score board drawn_numbers_set drawn_number
+
+let result2 =
+  let rec try_number numbers boards drawn_numbers_set last_number last_board =
+    match numbers, boards with
+    | [], _ -> None
+    | _, [] -> Some (Option.value_exn last_number, drawn_numbers_set, Option.value_exn last_board)
+    | drawn_number :: rest, boards ->
+      let drawn_numbers_set' = Set.add drawn_numbers_set drawn_number in
+      List.fold boards
+        ~init:{pending = []; wining = []}
+        ~f:(fun ({wining; pending} as acc) board ->
+            if is_wining_board board drawn_numbers_set'
+            then {acc with wining = board :: wining}
+            else {acc with pending = board :: pending})
+      |> (fun {wining; pending} ->
+          if List.is_empty wining
+          then try_number rest boards drawn_numbers_set' (Some drawn_number) last_board
+          else try_number rest (List.rev pending) drawn_numbers_set' (Some drawn_number) (List.hd wining))
+  in
+  match try_number numbers boards (Set.empty (module Int)) None None with
+  | None -> None
+  | Some (drawn_number, drawn_numbers_set, board) ->
+    calculate_score board drawn_numbers_set drawn_number
